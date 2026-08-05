335e097627e8ec8ab4a8ae2c2f2510684012e8fc
abhi070717 <abhibhawsar066@gmail.com>
2026-08-05 19:02:05 +0530

    Angular multi app components

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SignupComponent } from './signup/signup.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo:'/welcome',
+    pathMatch:'full'
+  },
+  {
+    path:'login',
+    component:LoginComponent
+  },
+  {
+    path:'welcome',
+    component:WelcomeComponent
+  },
+  {
+    path:'signup',
+    component:SignupComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
