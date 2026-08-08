bb49e2fe16e975dc44ff426ddb8435b8a6ff54d5
abhi070717 <abhibhawsar066@gmail.com>
2026-08-08 19:05:08 +0530

    signup and signin component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { HttpServiceService } from '../http-service.service';
 
 @Component({
   selector: 'app-login',
@@ -7,22 +8,47 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private httpService: HttpServiceService) { }
+
+  endpoint = 'http://localhost:8080/Auth/login';
 
   form: any = {
-
+    data: {},
     errorMsg: '',
     successMsg: ''
   }
 
   signIn() {
-    console.log(this.form.login);
-    console.log(this.form.password);
+    console.log(this.form.data.login);
+    console.log(this.form.data.password);
 
-    if (this.form.login == 'admin' && this.form.password =='admin') {
-      this.router.navigate(['/welcome']);
-    } else {
-      this.form.errorMsg = 'Invalid login or password';
-    }
+    let _self = this;
+    this.httpService.post(this.endpoint, this.form.data, function (response: any) {
+      console.log("response: ", response);
+
+      if (response.success == false && response.result.inputerror) {
+        _self.form.inputerror = response.result.inputerror;
+      }
+
+      if (response.success == false && response.result.message) {
+        _self.form.errorMsg = response.result.message;
+      }
+
+      if (response.success == true) {
+        _self.router.navigateByUrl('/welcome')
+      } else {
+        _self.form.errorMsg = 'Invalid login or password';
+      }
+
+    })
+    // if (this.form.login != null && this.form.password != null) {
+
+    //   this.router.navigateByUrl('/welcome');
+
+    // }
+  }
+
+  signUp() {
+    this.router.navigateByUrl('/signup');
   }
 }
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,23 +1,42 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { HttpServiceService } from '../http-service.service';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html'
 })
 export class SignupComponent {
 
-  constructor(private router: Router) { }
+  constructor(private httpService: HttpServiceService) { }
+
+  endpoint = 'http://localhost:8080/Auth/signUp';
 
   form: any = {
 
+    data: {},
     errorMsg: '',
-    successMsg: ''
+    successMsg: '',
+    inputerror: {},
   }
+
   signUp() {
-    console.log(this.form.firstName);
-    console.log(this.form.lastName);
-    console.log(this.form.login);
-    console.log(this.form.password);
-    console.log(this.form.dob);
+    let _self = this;
+    this.httpService.post(this.endpoint, this.form.data, function (response: any) {
+      console.log("response: ", response);
+
+      if (response.success == false && response.result.inputerror) {
+        _self.form.inputerror = response.result.inputerror;
+      }
+
+      if (response.success == false && response.result.message) {
+        _self.form.errorMsg = response.result.message;
+      }
+
+      if (response.success == true) {
+        _self.form.successMsg = response.result.message;
+      }
+
+    })
+
   }
 }
+
